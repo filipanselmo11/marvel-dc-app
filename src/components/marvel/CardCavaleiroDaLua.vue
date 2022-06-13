c29a4c6c226e933ae99faf643c4ef653ec62376d
Your Name <you@example.com>
2022-06-13 14:17:56 -0400

    Ronin E Viuva Negra

--- src/components/marvel/CardCavaleiroDaLua.vue
+++ src/components/marvel/CardCavaleiroDaLua.vue
@@ -3,7 +3,104 @@
     <v-img src="../../assets/cavaleiro-da-lua.png" max-height="460" max-width="400"></v-img>
     <v-card-title>Cavaleiro Da Lua</v-card-title>
     <v-card-actions>
-        <v-btn text>Explore</v-btn>
+        <v-dialog transition="dialog-bottom-transition" max-width="600">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on">Explore</v-btn>
+        </template>
+        <template v-slot:default="dialog">
+          <v-card>
+            <v-toolbar dark>Cavaleiro da Lua</v-toolbar>
+            <v-card-text>
+              <div class="mr-2 mt-3">
+                <strong>Nome Completo</strong>
+              </div>
+              <div class="mr-n2">Marc Spector</div>
+              <div class="mr-2 mt-3">
+                <strong>Codinomes Conhecidos</strong>
+              </div>
+              <div class="mr-n2">
+                Steven Grant, Jake Lockley, o Punho de Khonshu
+              </div>
+              <div class="mr-2 mt-3">
+                <strong>Terra Natal</strong>
+              </div>
+              <div class="mr-n2">Chicago, Illinois</div>
+              <div class="mr-2 mt-3">
+                <strong>Espécie</strong>
+              </div>
+              <div class="mr-n2">Humano transformado em Avatar do deus egípcio da Lua, Khonshu</div>
+              <div class="mr-2 mt-3">
+                <strong>Afiliações</strong>
+              </div>
+              <div class="mr-n2">
+                <v-timeline :dense="$vuetify.breakpoint.smAndDown">
+                  <v-timeline-item color="purple lighten-2" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="8"> Vingadores da Costa Oeste </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+
+                  <v-timeline-item color="amber lighten-1" fill-dot left small>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10"> Vingadores Secretos </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+
+                  <v-timeline-item color="cyan lighten-1" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10"> Defensores </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+
+                  <v-timeline-item color="red lighten-1" fill-dot left small>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10"> Marvel Knights </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+
+                  <v-timeline-item color="green lighten-1" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10"> Vingadores </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                  
+                  
+    
+                </v-timeline>
+              </div>
+              <div class="mr-2 mt-3">
+                <strong>Inimigo(s)</strong>
+              </div>
+              <div class="mr-n2">
+                Sem informações.
+              </div>
+            </v-card-text>
+            <v-card-actions class="justify-end">
+              <v-btn text @click="dialog.value = false">Fechar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
     </v-card-actions>
   </v-card>
 </template>
