983a04a25b831b104cbbe3e1272c7a6149c083f4
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-06-14 13:47:58 -0400

    Card SuperMan

--- src/components/dc/CardSuperman.vue
+++ src/components/dc/CardSuperman.vue
@@ -3,7 +3,81 @@
     <v-img src="../../assets/superman.jpeg" max-height="460" max-width="400"></v-img>
     <v-card-title>Superman</v-card-title>
     <v-card-actions>
-        <v-btn text>Explore</v-btn>
+        <v-dialog transition="dialog-bottom-transition" max-width="600">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on">Explore</v-btn>
+        </template>
+        <template v-slot:default="dialog">
+          <v-card>
+            <v-toolbar dark>Superman</v-toolbar>
+            <v-card-text>
+              <div class="mr-2 mt-3">
+                <strong>Nome Completo</strong>
+              </div>
+              <div class="mr-n2">Clark Joseph Kent(adotado) / Kal-El</div>
+              <div class="mr-2 mt-3">
+                <strong>Terra Natal</strong>
+              </div>
+              <div class="mr-n2">Krypton</div>
+              <div class="mr-2 mt-3">
+                <strong>Espécie</strong>
+              </div>
+              <div class="mr-n2">kryptoniano</div>
+              <div class="mr-2 mt-3">
+                <strong>Afiliações</strong>
+              </div>
+              <div class="mr-n2">
+                <v-timeline :dense="$vuetify.breakpoint.smAndDown">
+                  <v-timeline-item color="purple lighten-2" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="8">Planeta Diário</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+
+                  <v-timeline-item color="deep-purple accent-2" fill-dot left small>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Liga da Justiça</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                  <v-timeline-item color="red darken-3" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="8">Legião dos Super-Heróis</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                   <v-timeline-item color="blue darken-3" fill-dot left small>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Liga da Justiça</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                </v-timeline>
+              </div>
+              <div class="mr-2 mt-3">
+                <strong>Inimigo(s)</strong>
+              </div>
+              <div class="mr-n2">Sem informação</div>
+            </v-card-text>
+            <v-card-actions class="justify-end">
+              <v-btn text @click="dialog.value = false">Fechar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
     </v-card-actions>
   </v-card>
 </template>
