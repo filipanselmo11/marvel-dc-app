cdc9b1ef018d112c48865a408214e124266e37da
Your Name <you@example.com>
2022-06-14 10:47:32 -0400

    Card Batman e The Flash

--- src/components/dc/CardBatman.vue
+++ src/components/dc/CardBatman.vue
@@ -3,17 +3,103 @@
     <v-img src="../../assets/batman.png" max-height="460" max-width="400"></v-img>
     <v-card-title>Batman</v-card-title>
     <v-card-actions>
-        <v-btn text>Explore</v-btn>
+      <v-dialog transition="dialog-bottom-transition" max-width="600">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on">Explore</v-btn>
+        </template>
+        <template v-slot:default="dialog">
+          <v-card>
+            <v-toolbar dark>Batman</v-toolbar>
+            <v-card-text>
+              <div class="mr-2 mt-3">
+                <strong>Nome Completo</strong>
+              </div>
+              <div class="mr-n2">Bruce Thomas Wayne</div>
+              <div class="mr-2 mt-3">
+                <strong>Codinomes Conhecidos</strong>
+              </div>
+              <div class="mr-n2">
+                Homem-Morcego, O Cavaleiro das Trevas, O Cruzado Encapuzado, O Detetive
+                das Sombras, O Maior Detetive do Mundo
+              </div>
+              <div class="mr-2 mt-3">
+                <strong>Terra Natal</strong>
+              </div>
+              <div class="mr-n2">Gotham City</div>
+              <div class="mr-2 mt-3">
+                <strong>Espécie</strong>
+              </div>
+              <div class="mr-n2">Sem informação.</div>
+              <div class="mr-2 mt-3">
+                <strong>Afiliações</strong>
+              </div>
+              <div class="mr-n2">
+                <v-timeline :dense="$vuetify.breakpoint.smAndDown">
+                  <v-timeline-item color="purple lighten-2" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="8">Fundação Wayne</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+
+                  <v-timeline-item color="deep-purple accent-2" fill-dot left small>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Liga da Justiça</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                  <v-timeline-item color="black" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Renegados</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                  <v-timeline-item color="red" fill-dot left small>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Bat-Familia</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                  <v-timeline-item color="white" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Corporação Batman</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                </v-timeline>
+              </div>
+              <div class="mr-2 mt-3">
+                <strong>Inimigo(s)</strong>
+              </div>
+              <div class="mr-n2">Sem informação</div>
+            </v-card-text>
+            <v-card-actions class="justify-end">
+              <v-btn text @click="dialog.value = false">Fechar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
