b75add701210f13282c8a58fa6ca884b8e3458d6
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-06-15 14:34:08 -0400

    Super Choque e Raio Negro

--- src/components/dc/CardRaioNegro.vue
+++ src/components/dc/CardRaioNegro.vue
@@ -3,17 +3,66 @@
     <v-img src="../../assets/raio-negro-dc.png" max-height="460" max-width="400"></v-img>
     <v-card-title>Raio Negro</v-card-title>
     <v-card-actions>
-        <v-btn text>Explore</v-btn>
+      <v-dialog transition="dialog-bottom-transition" max-width="600">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on">Explore</v-btn>
+        </template>
+        <template v-slot:default="dialog">
+          <v-card>
+            <v-toolbar dark>Raio Negro</v-toolbar>
+            <v-card-text>
+              <div class="mr-2 mt-3">
+                <strong>Nome Completo</strong>
+              </div>
+              <div class="mr-n2">Jefferson Pierce</div>
+              <div class="mr-2 mt-3">
+                <strong>Afiliações</strong>
+              </div>
+              <div class="mr-n2">
+                <v-timeline :dense="$vuetify.breakpoint.smAndDown">
+                  <v-timeline-item color="purple lighten-2" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="8">Liga da Justiça</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+
+                  <v-timeline-item color="white" fill-dot left small>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Renegados</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                  <v-timeline-item color="black" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">C.O.I</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                </v-timeline>
+              </div>
+            </v-card-text>
+            <v-card-actions class="justify-end">
+              <v-btn text @click="dialog.value = false">Fechar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/dc/CardSuperChoque.vue
+++ src/components/dc/CardSuperChoque.vue
@@ -3,7 +3,128 @@
     <v-img src="../../assets/super-choque.jpeg" max-height="460" max-width="400"></v-img>
     <v-card-title>Super Choque</v-card-title>
     <v-card-actions>
-        <v-btn text>Explore</v-btn>
+        <v-dialog transition="dialog-bottom-transition" max-width="600">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on">Explore</v-btn>
+        </template>
+        <template v-slot:default="dialog">
+          <v-card>
+            <v-toolbar dark>Super Choque</v-toolbar>
+            <v-card-text>
+              <div class="mr-2 mt-3">
+                <strong>Nome Completo</strong>
+              </div>
+              <div class="mr-n2">
+                Virgil Ovid Hawkins
+              </div>
+               <div class="mr-2 mt-3">
+                <strong>Terra Natal</strong>
+              </div>
+              <div class="mr-n2">
+                Dakota
+              </div>
+              <div class="mr-2 mt-3">
+                <strong>Espécie</strong>
+              </div>
+              <div class="mr-n2">
+                Meta-Humano
+              </div>
+              <div class="mr-2 mt-3">
+                <strong>Afiliações</strong>
+              </div>
+              <div class="mr-n2">
+                <v-timeline :dense="$vuetify.breakpoint.smAndDown">
+                  <v-timeline-item color="purple lighten-2" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="8">Heroes</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+
+                  <v-timeline-item color="white" fill-dot left small>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Jovens Titãs</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                  <v-timeline-item color="black" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Liga da Justiça</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                  <v-timeline-item color="orange" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Shadow Cabinet</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                  <v-timeline-item color="cyan" fill-dot left small>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Partner of Dusk</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                  <v-timeline-item color="yellow" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Esquadra da Cesta</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                  <v-timeline-item color="orange" fill-dot left small>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Raça Noturna</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                  <v-timeline-item color="grey" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Icone e Rocket</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                  <v-timeline-item color="teal" fill-dot right>
+                    <v-card>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" md="10">Hardware</v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card>
+                  </v-timeline-item>
+                </v-timeline>
+              </div>
+            </v-card-text>
+            <v-card-actions class="justify-end">
+              <v-btn text @click="dialog.value = false">Fechar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
     </v-card-actions>
   </v-card>
 </template>
